78a4daa2fc0c7f3356a9c8cd4a5fb2220f4889c3
yb00 <yagobraz98@gmail.com>
2021-06-29 23:53:31 -0300

    Fix layout

--- charts.r
+++ charts.r
@@ -10,10 +10,25 @@ library(extrafont)
 
 loadfonts(quiet = T)
 
-mycolors = c("#8ACCFF", "#80C8FF", "#77C4FF", "#6DC0FF", "#64BCFF", "#5AB7FF", "#51B3FF", "#47AFFF", "#3EABFF", "#34A7FF")
+draw_key_polygon3 <- function(data, params, size) {
+  lwd <- min(data$size, min(size) / 4)
+  
+  grid::rectGrob(
+    width = grid::unit(0.8, "npc"),
+    height = grid::unit(0.8, "npc"),
+    gp = grid::gpar(
+      col = data$colour,
+      fill = alpha(data$fill, data$alpha),
+      lty = data$linetype,
+      lwd = lwd * .pt,
+      linejoin = "mitre"
+    ))
+}
+
+my_colors = c("#e1f5f3", "#b3e5fc", "#81d4fa", "#4fc3f7", "#29b6f6", "#03a9f4", "#039be5", "#0288d1", "#0277bd", "#01579b")
 
 test = scale_fill_gradient2(low = "#0000FF", mid = "#FFFFFF", high ="#FF0000", 
-                     midpoint = median(v$value), space = "rgb", guide = "colourbar")
+                            midpoint = median(v$value), space = "rgb", guide = "colourbar")
 
 df <- read.csv(file='./ssim.csv', sep = ';', fileEncoding = "UTF-8-BOM")
 df <- df %>% unite("with_on", "filter_name":"noise_intensity", remove = TRUE)
@@ -43,46 +58,38 @@ plotted <- data.frame('Files' = categories,
 plotted.m <- melt(plotted, id.vars='Files')
 
 # plotted.m$variable = as.character(plotted.m$variable)
+# plotted.m$variable = as.numeric(readr::parse_number(plotted.m$variable, locale = readr::locale(decimal_mark = ",")))
 plotted.m$value = as.numeric(readr::parse_number(plotted.m$value, locale = readr::locale(decimal_mark = ",")))
 
 chart_name = df[lines[1], 1]
+filename = paste(chart_name, ".png", sep = '')
 ggplot(plotted.m, aes(x=Files, y=value, group=variable, fill=variable)) +
   ggtitle("Filtro Gaussiano - Ruído Sal e Pimenta 50%") +
+  xlab("Imagens") +
+  ylab("SSIM") +
   geom_col(
     aes(fill=variable),
-    # colour = "black",
-    width = 0.8,
-    position = position_dodge2(width=0.8, preserve = "single"),
-    ) +
-  scale_fill_manual(
-    name = "Variável",
-    values = mycolors
-    ) +
+    colour = "black",
+    width = 0.5,
+    position = position_dodge(width = 0.8),
+    key_glyph = "polygon3"
+  ) +
+  scale_fill_manual(name = "Variável", values = my_colors) +
+  theme_light() +
   theme(
-    plot.title = element_text(size=20, family = "Cambria", face = "bold", hjust = 0.5),
-    axis.text.y = element_text(family = "Calibri", colour = "black", size = 12, vjust = 0.3),
-    axis.text.x = element_text(family = "Calibri", colour = "black", size = 12, angle = 65, hjust = 1)
-    )
-png(filename = chart_name + ".png")
+    plot.title = element_text(size=24, family = "Cambria", face = "bold", hjust = 0.5, margin = margin(20, 0, 20, 0)),
+    axis.title.x = element_text(family = "Calibri", size = 16, face = "bold", margin = margin(10, 0, 10, 0)),
+    axis.title.y = element_text(family = "Calibri", size = 16, face = "bold", margin = margin(0, 10, 0, 10)),
+    axis.text.y = element_text(family = "Calibri", size = 14, colour = "black", vjust = 0.5, margin = margin(0, 5, 0, 0)),
+    axis.text.x = element_text(family = "Ubuntu Mono", size = 16, colour = "black",angle = 45, hjust = 1),
+    legend.title = element_text(family = "Calibri", colour = "black", size = 16, hjust = 0.5),
+    legend.text = element_text(family = "Calibri", colour = "black", size = 12),
+    legend.direction = "vertical",
+    legend.position = "right",
+  )
+
+
+ggsave(f)
+plot(plt)
 # lines <- lines + 10
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+dev.off()
